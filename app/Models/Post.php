980bb2b9c538f439490ade89493b3fc5cb6e3f87
MICHAEL RIO <michaelrio89@gmail.com>
2024-12-12 15:53:28 +0700

    EDIT: change syntax to object based, rename tables in migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = 'elynx_posts';
+    // to define what column allowed to fill
     protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- resources/views/value.blade.php
+++ resources/views/value.blade.php
@@ -3,14 +3,14 @@
     <x-slot:post_title>{{ $title }}</x-slot>
 
     <article class="py-5 max-w-screen-md">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $result['title'] }}</h2>
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $result->title }}</h2>
 
         <div class="text-base text-gray-500">
-            <a href="#">{{ $result['author'] }}</a> | {{ $result['created_at']->diffForHumans() }}
+            <a href="#">{{ $result->author }}</a> | {{ $result->created_at->diffForHumans() }}
         </div>
 
         <p class="my-4 font-light">
-            {{ $result['body'] }}
+            {{ $result->body }}
         </p>
 
         <a href="/posts" class="font-medium text-blue-500 hover:underline">Back To Post &laquo;</a>
