688b1202e97f24c14ecc9a8a34f366b03f4673c9
MICHAEL RIO <michaelrio89@gmail.com>
2024-12-12 21:11:34 +0700

    ADD: created factory for test bulk insert database

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+
+    //  to define tables_name
     protected $table = 'elynx_posts';
     // to define what column allowed to fill
-    protected $fillable = ['title', 'author', 'slug', 'body'];
+    protected $fillable = [
+        'title', 
+        'author', 
+        'slug', 
+        'body'
+    ];
 }
